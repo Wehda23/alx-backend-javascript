14b7c76727cce221bc460f3c37ed6ac547b64e3a
Waheed Khaled <waheedkhaled95@gmail.com>
2024-06-26 15:02:37 +0300

    Task 10 Done

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,5 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  array.forEach((value) => newArray.push(`${appendString + value}`));
+  return newArray;
+}
