a777418f462992f180a1c4428d9f69888e05e3ba
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-02 13:43:56 +0300

    Updates

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,3 +1,4 @@
 export default function setFromArray(arr) {
+  if(!(arr instanceof Array)) return new Set();
   return new Set(arr);
 }
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,8 @@
 const hasValuesFromArray = (set, array) => {
   if (!(set instanceof Set)) return false;
   if (!(array instanceof Array)) return false;
+  // Check the size and length of the array and set
+  if (set.size === 0 || array.length === 0) return false;
   // loop over array
   for (const number of array) {
     // if number is in set, return true
