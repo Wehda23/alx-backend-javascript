680cdabb8d45402c1b6fdee830ecace6992f8c42
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-02 14:36:45 +0300

    test asnwer

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 const cleanSet = (set, startString) => {
   if (set.size === 0 || startString.length === 0) return '';
+  if (!(set instanceof Set) || typeof startString !== 'string') return '';
   return Array.from(set).filter((value) => value.startsWith(startString)).map((value) => value.replace(startString, '')).join('-');
 };
 
