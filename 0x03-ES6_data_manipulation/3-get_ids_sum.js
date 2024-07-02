d4da7aaf9cdc84a53f4fd4fa466c4f397045c8d5
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-02 09:07:49 +0300

    Few updates

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,8 @@
-const getStudentIdsSum = (students) => students.reduce((acc, cur) => acc + cur.id, 0);
+const getStudentIdsSum = (students) => {
+  if (!(students instanceof Array)) {
+    return [];
+  }
+  return students.reduce((acc, value) => acc + value.id, 0);
+};
 
 export default getStudentIdsSum;
