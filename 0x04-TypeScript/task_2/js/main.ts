c74a02e95edbef3e9cb9454fa272e1a7ee9b943b
Waheed Khaled <waheedkhaled95@gmail.com>
2024-07-04 03:57:20 +0300

    Task 2 done

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,3 +34,48 @@ const createEmployee: createEmployeeInterface = (salary: string | number): Direc
 
     return new Director();
 }
+
+
+const teacher: Director | Teacher = createEmployee("499")
+const director: Director | Teacher = createEmployee(501)
+
+console.log(teacher.workFromHome())
+console.log(director.workFromHome())
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+
+interface isDirectorInterface {
+    (employee: Director | Teacher): boolean
+}
+
+const isDirector: isDirectorInterface = (employee: Director | Teacher): boolean => {
+    return employee instanceof Director;
+}
+
+interface executeWorkInterface {
+    (employee: Director | Teacher): string
+}
+
+const executeWork: executeWorkInterface = (employee: Director | Teacher): string => {
+    if (isDirector(employee)) {
+        return (employee as Director).workDirectoryTasks()
+    }
+    return (employee as Teacher).workTeacherTasks()
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+type Subjects = "Math" | "History"
+
+interface TeachClassInterface {
+    (subject: Subjects): string
+}
+
+const teachClass: TeachClassInterface = (subject: Subjects): string => {
+    return `Teaching ${subject}`
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
